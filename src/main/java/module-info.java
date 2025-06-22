df4c56c9d68820dc584e28a7927e7565602ebec1
Luis Malagon <luismalagon123@gmail.com>
2025-06-22 18:39:18 -0400

    Removed module-info.java since we no longer need javafx

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.midtermproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.midtermproject to javafx.fxml;
-    exports com.example.midtermproject;
-}
